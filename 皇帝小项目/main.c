ae06a52c949a1f6c0a0da51f628e8cddd4ac3051
hanxx <myhanguoxing@126.com>
2018-03-21 15:52:14 +0800

    更新小项目

--- 皇帝小项目/main.c
+++ 皇帝小项目/main.c
@@ -1,157 +1,147 @@
 #include <stdio.h>
-//播放音频 还需要在链接中添加 “-lwinmm”
-#include <windows.h>
-#include <mmsystem.h>
-#pragma comment(lib,"Winmm.lib")
-#include <string.h> //支持字符串操作
-//常量
-#define MAX 6
+#include <string.h>
 
-int main()
-{
+#define MAX 6   //定义宏观变量
+/*
+ * 皇帝的养成游戏
+ * */
+int main() {
+
+    int i, j, temp;  //循环变量 临时变量
+    int count = 5;  //妃子个数
+    char empName[20];  //皇帝姓名
+    int choise;     //选择 1234
+    int searchIndex = -1;    //定义一个参数，用来判断是否存在
+    char tempName[20];  //临时变量
 
 
-    //文件类型只能是波形文件 只能为wav
-    //PlaySound(TEXT("sounds\\1.wav"),NULL,SND_FILENAME | SND_ALIAS | SND_LOOP);
+    //定义后宫 默认5个 可选一个
+    char names[MAX][20] = {"赵飞燕", "小乔", "西施", "貂蝉", "杨玉环"}; //名讳
+    char levelNames[5][10] = {"才人", "贵人", "嫔妃", "贵妃", "皇后"};   //等级
+    int levels[MAX] = {2, 0, 1, 3, 3, -1};   //names对应的等级 -1 为预留位置
+    int loves[MAX] = {100, 100, 100, 100, 100, -1};    //好感度 默认初始化为100
 
-    int i , j, temp;  //循环变量
-    int count = 5; //默认的妃子数
-    char ename[50];   //定义年号
-    int choise;       //选项
-    int searchIndex = -1;   //查找打入冷宫的
-    char tempName[20];  //翻牌的临时变量
-    //选妃的数组 6是有6个嫔妃，名字是20长度
-    char names[MAX][20]={"貂蝉","杨贵妃","赵飞燕","西施","小乔"};     //初始化五个
-    //级别数组
-    char levelNames[5][10]={"才人","贵人","嫔妃","贵妃","皇后"};
-    //用来存放每个妃子的级别 貂蝉--贵人 -1用来占位 用来添加一个
-    int levels[MAX]={1,3,2,0,0,-1};
-    //用来对应好感度 初始为100
-    int loves[MAX]={100,100,100,100,100,-1};
-
-    printf("后宫\n");
-    printf("姓名\t级别\t好感度\n");
-    for(i=0;i<count;i++){
-        printf("%s\t%s\t%d\n",names[i], levelNames[levels[i]], loves[i]);
+    printf("==========================================\n");
+    printf("%-10s\t\t%-10s\t\t%-10s\n", "名讳", "称谓", "好感度");
+    for (i = 0; i < count; i++) {
+        printf("%-10s\t\t%-10s\t\t%-10d\n", names[i], levelNames[levels[i]], loves[i]);
     }
-    printf("-----------------------------------------\n");
+    printf("==========================================\n");
 
-    printf("皇上万岁，请给您自己起一个年号:");
-    //得到年号 ，注意不用 & 因为是char类型
-    scanf("%s",ename);
-    printf("%s驾到，吾皇万岁！\n",ename);
-
-    printf("1、新皇登基，皇帝选妃\t\t(增加)\n");
-    printf("2、皇帝翻牌;宠幸爱妃\t\t(修改)\n");
-    printf("3、皇帝大怒，要将爱妃打入冷宫\t\t(删除)\n");
-    printf("4、皇帝要单独带爱妃进入小树林 嘿嘿嘿\n");
-    printf("%s 请做出您的选择:\n",ename);
-    scanf("%d",&choise);
-    switch(choise){
-        case 1: //增加
-            if(count<MAX){
-                printf("请输入您选妃的名字：");
-                scanf("%s",names[count]);
-                //将数组最后一个初始化
-                levels[count]=0;    //级别
-                loves[count]=100;   //好感度
-                count++;
-            }else{
-                printf("后宫已经人满了，住不下了\n添加失败\n");
-            }
-
-            break;
-        case 2: //查找 --修改
-            //好感度+20,级别+1 其他人好感度减10
-            //查找
-            printf("请输入您要宠幸的妃子名称:");
-            scanf("%s",tempName);
-            //字符串查找strcmp  引入<string.h>
-            //strcmp =0 字符串相等  1 前一个大于后一个 -1 前一个小于后一个
-            for(i = 0; i < count; i++){
-                if(strcmp(tempName,names[i]) == 0){
-                    searchIndex = i;
+    printf("先皇驾崩！新皇登基！请为您定一个年号：\n");
+    //注意：这里因为是字符串，所以不用 &
+    scanf("%s", empName);
+    printf("%s驾到，万岁万岁万万岁\n", empName);
+    for (int day = 0; day < 10; day++) {
+        printf("第%d天\n",day+1);
+        printf("请皇上进行选择操作：\n");
+        printf("1.新皇登基！下旨选妃：\n");
+        printf("2.皇帝翻牌，宠幸妃子：\n");
+        printf("3.皇帝生气，打入冷宫：\n");
+        printf("4.皇帝想带妃子去小树林：\n");
+        scanf("%d", &choise);    //皇帝选择
+        switch (choise) {
+            case 1:
+                //先判断妃子的个数是否已经满了
+                if (count < MAX) {
+                    printf("陛下，您决定选妃了吗，如果决定了，请输入您想选的妃子名号：\n");
+                    scanf("%s", names[count]);
+                    //所有数组初始化
+                    levels[count] = 0; //等级
+                    loves[count] = 100; //好感度
+                    count++;
+                    //
+                } else {
+                    printf("陛下，后宫住满了，注意节制啊！");
                 }
-            }
-            if(searchIndex==-1){
-                printf("皇上是不是想要选妃了，后宫暂时还没有这个妃子\n");
-            }else{
-                for(i = 0; i < count; i++){
-                    //如果发现这个妃子
-                    if(strcmp(tempName,names[i]) == 0){
-                        loves[i] += 20;
-                        //级别不能超过4
-                        levels[i] = levels[i] > 4 ? 4 : levels[i]+1;
-                    }else
-                        loves[i] -= 10;
+                break;
+            case 2: //字符串查找 引入 string.h
+                printf("陛下，请问您·今晚准备临幸哪位妃子：\n");
+                scanf("%s", tempName);
+                //字符串查找  strcmp  如果=0 相等， =1 前一个字符串>后一个 =-1 前一个字符串<后一个
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(tempName, names[i]) == 0) {
+                        searchIndex = i;
+                        break;
+                    }
                 }
-            }
+                if (searchIndex == -1) {
+                    printf("陛下！您是不想想多了，后宫没有这个妃子啊\n");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        if (strcmp(tempName, names[i]) == 0) {
+                            loves[i] += 20; //好感度+20
+                            levels[i] = levels[i] >= 4 ? 4 : levels[i] + 1; //等级是不是大于最大值，如果是 保持 或者+1
 
-            break;
-        case 3: //删除一个 ，后面一个放到前面
-
-            printf("皇上,哪个妃子惹您了，需要打入冷宫！\n");
-
-            scanf("%s",tempName);
-
-            //循环查找
-            for(i = 0; i < count; i++){
-                if(strcmp(tempName,names[i]) == 0){
-                    //记录下标
-                    searchIndex = i;
-                    break;
+                        } else {
+                            loves[i] -= 10;  //未被选中的好感度都减1011
+                        }
+                    }
+                    searchIndex = -1; //恢复默认值，方便下面调用
                 }
-            }
-            //初始值指没有找到
-            if(-1 == searchIndex){
-                printf("皇上可能迷糊了，压根都没这个嫔妃！虚惊一场\n");
-            }else{
-                for(i=searchIndex;i<count-1;i++){
+
+                break;
+            case 3: //删除数组
+
+                printf("陛下！哪位爱妃惹您了，需要打入冷宫：\n");
+
+                scanf("%s", tempName);
+
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(tempName, names[i]) == 0) {
+                        searchIndex = i;
+                        break;
+                    }
+                }
+
+                if (searchIndex == -1) {
+                    printf("皇上可能迷糊了，压根都没这个嫔妃！虚惊一场\n");
+                } else {
+                    for (i = searchIndex; i < count - 1; i++) {
                         //需要strcpy函数 实现数组的赋值
-                        strcpy(names[i],names[i+1]);
-                        loves[i]=loves[i+1];
-                        loves[i]=loves[i+1];
+                        strcpy(names[i], names[i + 1]);
+                        loves[i] = loves[i + 1];
+                        loves[i] = loves[i + 1];
+                    }
+                    searchIndex = -1; //恢复默认值，方便下面调用
+                    count--;
                 }
-                count--;
-            }
-            break;
-        case 4:
-            printf("44444444444\n");
-            break;
-        default:
-            printf("%s,还请慎重选择啊！！！！\n",ename);
-    }
+                break;
+            case 4:
+                printf("444");
+                break;
+            default:
+                printf("皇上！请慎重啊，您可没有那么玩的\n");
+                break;
+        }
 
-    //打印结果按照级别进行冒泡排序
-    for(i=0;i<count-1;i++){
-
-        for (j = 0; j< count-i-1; j++) {
-
-            if (levels[j] <levels[j+1]){
+        //打印结果按照级别进行冒泡排序
+        for (i = 0; i < count - 1; i++) {
+            for (j = 0; j < count - i - 1; j++) {
+                if (levels[j] < levels[j + 1]) {
                     //交换姓名 级别 好感度
-                temp=levels[j];
-                levels[j]=levels[j+1];
-                levels[j+1]=temp;
+                    temp = levels[j];
+                    levels[j] = levels[j + 1];
+                    levels[j + 1] = temp;
 
-                temp= loves[j];
-                loves[j]=loves[j+1];
-                loves[j+1]=temp;
+                    temp = loves[j];
+                    loves[j] = loves[j + 1];
+                    loves[j + 1] = temp;
 
-                strcpy(tempName,names[j]);
-                strcpy(names[j],names[j+1]);
-                strcpy(names[j+1],tempName);
+                    strcpy(tempName, names[j]);
+                    strcpy(names[j], names[j + 1]);
+                    strcpy(names[j + 1], tempName);
+                }
             }
         }
-    }
-    printf("-----------------------------------------\n");
-    printf("操作完之后的结果：\n");
-    printf("姓名\t级别\t好感度\n");
-    for(i=0;i<count;i++){
-        printf("%s\t%s\t%d\n",names[i], levelNames[levels[i]], loves[i]);
-    }
-    printf("-----------------------------------------\n");
 
-
+        printf("==========================================\n");
+        printf("%-10s\t\t%-10s\t\t%-10s\n", "名讳", "称谓", "好感度");
+        for (i = 0; i < count; i++) {
+            printf("%-10s\t\t%-10s\t\t%-10d\n", names[i], levelNames[levels[i]], loves[i]);
+        }
+        printf("==========================================\n");
+    }
 
 
     return 0;
